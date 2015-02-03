51901fe270ee9c1d9869693c83451ddc35bbbf7f
Vinod Gupta <vinod_gupta@live.com>
2015-02-03 12:00:33 -0800

    Initial Commit

---
+++ StringFromPhone/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StringFromPhone
+//
+//  Created by Vinod Gupta on 2/3/15.
+//  Copyright (c) 2015 Vinod Gupta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
